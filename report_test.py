17f525747c8e98c5955e945e36142ba11204c4fb
vzvu3k6k <vzvu3k6k@gmail.com>
2020-05-23 12:59:44 +0900

    Add test

---
+++ report_test.py
@@ -0,0 +1,36 @@
+import unittest
+from report import Report
+from signal_color import SignalColor
+from datetime import date
+
+class TestReport(unittest.TestCase):
+  def test_信号色(self):
+    report = self.__prepare_report('normal')
+    self.assertEqual(report.信号色(), SignalColor.GREEN)
+  
+  def test_日付(self):
+    report = self.__prepare_report('normal')
+    self.assertEqual(report.日付(), date(2020, 5, 22))
+
+  def test_感染経路不明者の前週増加比(self):
+    report = self.__prepare_report('normal')
+    self.assertEqual(report.感染経路不明者の前週増加比(), 0.59)
+
+  def test_感染経路不明者(self):
+    report = self.__prepare_report('normal')
+    self.assertEqual(report.感染経路不明者(), 1.43)
+
+  def test_確定診断検査における陽性率(self):
+    report = self.__prepare_report('normal')
+    self.assertEqual(report.確定診断検査における陽性率(), 0.004)
+
+  def test_患者受入重症病床使用率(self):
+    report = self.__prepare_report('normal')
+    self.assertEqual(report.患者受入重症病床使用率(), 0.154)
+
+  def __prepare_report(self, fixture_name):
+    with open(f'fixtures/{fixture_name}.html', encoding='CP932') as f:
+      return Report(f.read())
+
+if __name__ == '__main__':
+  unittest.main()
