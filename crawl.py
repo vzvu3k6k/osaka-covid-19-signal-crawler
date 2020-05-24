d7ed53bfd6327fa2fd12f0d11be46cea03e9bb74
vzvu3k6k <vzvu3k6k@gmail.com>
2020-05-24 20:49:36 +0900

    Save to gh-pages

--- crawl.py
+++ crawl.py
@@ -1,4 +1,6 @@
+import os
 import http.client
+import github
 from report import Report
 
 def fetch_page():
@@ -9,3 +11,27 @@ def fetch_page():
     raise RuntimeError(f"HTTP request failed (status: {response.status})")
   return response.read().decode('CP932')
 
+def get_repository(token, repository):
+  return github.Github(token).get_repo(repository)
+
+def save_json(repository, report, committer):
+  return repository.create_file(
+    path=f"/v1/{report.filename()}",
+    content=report.json(),
+    message="Update",
+    branch="gh-pages",
+    committer=committer,
+  )
+
+html = fetch_page()
+report = Report(html)
+
+repository = get_repository(
+  os.environ.get("GITHUB_TOKEN"),
+  os.environ.get("GITHUB_REPOSITORY")
+)
+committer = github.InputGitAuthor(
+  os.environ.get("GIT_COMMITTER_NAME"),
+  os.environ.get("GIT_COMMITTER_EMAIL")
+)
+save_json(repository, report, committer)
