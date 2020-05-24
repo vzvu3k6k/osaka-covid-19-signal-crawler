af192eecdc8367ffc3b399f7536af9c1fe71ca77
vzvu3k6k <vzvu3k6k@gmail.com>
2020-05-24 14:25:32 +0900

    Add signal_color_test.py

---
+++ signal_color_test.py
@@ -0,0 +1,14 @@
+import unittest
+from signal_color import SignalColor
+
+class TestSignalColor(unittest.TestCase):
+  def test_stringify(self):
+    self.assertEqual(SignalColor.stringify(SignalColor.RED), 'red')
+  
+  def test_parse(self):
+    self.assertEqual(SignalColor.parse('赤'), SignalColor.RED)
+    with self.assertRaises(RuntimeError):
+      SignalColor.parse('白')
+
+if __name__ == '__main__':
+  unittest.main()
