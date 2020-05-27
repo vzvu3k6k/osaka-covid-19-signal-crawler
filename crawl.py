f3e88dfcce6f62486434bfe3051154521185e421
vzvu3k6k <vzvu3k6k@gmail.com>
2020-05-27 09:11:54 +0900

    Fix packaging

--- crawl.py
+++ crawl.py
@@ -1,8 +1,8 @@
 import os
 import http.client
 import github
-from .report import Report
-from .report_json_presenter import ReportJsonPresenter
+from signal_crawler.report import Report
+from signal_crawler.report_json_presenter import ReportJsonPresenter
 
 def fetch_page():
   conn = http.client.HTTPConnection("www.pref.osaka.lg.jp")
