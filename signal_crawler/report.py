40fa9a3ae63302ba0fa94c08641719bb2524419c
vzvu3k6k <vzvu3k6k@gmail.com>
2020-06-04 01:54:06 +0900

    Remove regexp matching from Report for robustness
    against broken HTML.
    
    This removes a workaround of #14.

--- signal_crawler/report.py
+++ signal_crawler/report.py
@@ -15,131 +15,32 @@ class Report:
     return SignalColor.parse(match.group(1))
 
   def date(self):
-    text = self.__details().group('date')
+    pattern = re.compile(r'\d+月\d+日')
+    text = self.__detail_node().find(string=pattern)
+    text = re.search(pattern, text).group(0)
     return datetime.strptime(text, '%m月%d日').date().replace(year=self.year)
   
   def 感染経路不明者の前週増加比(self):
-    return float(self.__details().group('感染経路不明者の前週増加比'))
+    return float(self.__find_detail_value('①感染経路不明者の前週増加比'))
 
   def 感染経路不明者(self):
-    text = self.__details().group('感染経路不明者')
+    text = self.__find_detail_value('②感染経路不明者数')
     match = re.search(r'([\d+\.]+)人', text)
     return float(match.group(1))
 
   def 確定診断検査における陽性率(self):
-    text = self.__details().group('確定診断検査における陽性率')
+    text = self.__find_detail_value('③確定診断検査における陽性率')
     match = re.search(r'([\d+\.]+)%', text)
     return float(match.group(1)) * 0.01
 
   def 患者受入重症病床使用率(self):
-    text = self.__details().group('患者受入重症病床使用率')
+    text = self.__find_detail_value('④患者受入重症病床使用率')
     match = re.search(r'([\d+\.]+)%', text)
     return float(match.group(1)) * 0.01
 
-  def __details(self):
-    details = self.soup.find(class_ = 'shingo-meisai-waku').prettify()
-    pattern = r'''<div class="shingo-meisai-waku">
- <table>
-  <tbody>
-   <tr>
-    <th(?:.+?)>
-     大阪モデル・モニタリング指標
-    </th>
-    <th(?:.+?)>
-     (?P<date>.+)
-     <br/>
-     現在
-    </th>
-    <th(?:.+?)>
-     自粛要請等の基準
-    </th>
-    <th(?:.+?)>
-     自粛解除の基準
-    </th>
-   </tr>
-   <tr>
-    <th(?:.+?)>
-     分析事項
-    </th>
-    <th(?:.+?)>
-     内容\(※\)
-    </th>
-   </tr>
-   <tr>
-   </tr>
-   <tr>
-    <td(?:.+?)>
-     （１）市中での感染拡大状況
-    </td>
-    <td>
-     ①感染経路不明者の前週増加比
-    </td>
-    <td(?:.+?)>
-     (?P<感染経路不明者の前週増加比>.+)
-    </td>
-    <td(?:.+?)>
-     1以上
-    </td>
-    <td(?:.+?)>
-     －
-    </td>
-   </tr>
-   <tr>
-    <td>
-     ②感染経路不明者数
-    </td>
-    <td(?:.+?)>
-     (?P<感染経路不明者>.+)
-    </td>
-    <td(?:.+?)>
-     5から10人以上
-    </td>
-    <td(?:.+?)>
-     10人未満
-    </td>
-   </tr>
-   <tr>
-    <td>
-     （２）新規陽性患者の発生状況
-     <br/>
-     ・検査体制のひっ迫状況
-    </td>
-    <td>
-     ③確定診断検査における陽性率
-    </td>
-    <td(?:.+?)>
-     (?P<確定診断検査における陽性率>.+)
-    </td>
-    <td(?:.+?)>
-     7%以上
-    </td>
-    <td(?:.+?)>
-     7%未満
-    </td>
-   </tr>
-   <tr>
-    <td>
-     （３）病床のひっ迫状況
-    </td>
-    <td>
-     ④患者受入重症病床使用率
-    </td>
-    <td(?:.+?)>
-     (?P<患者受入重症病床使用率>.+)
-    </td>(?:
-    <td>
-    </td>)?
-    <td(?:.+?)>
-     －
-    </td>
-    <td(?:.+?)>
-     60%未満
-    </td>
-   </tr>
-  </tbody>
- </table>
- <div(?:.+?)>
-  ※病床使用率以外の指標は７日間移動平均。
-  <br/>
- </div>'''
-    return re.search(pattern, details)
+  def __find_detail_value(self, label):
+    header = self.__detail_node().find('td', string=label)
+    return header.find_next_sibling('td').string
+
+  def __detail_node(self):
+    return self.soup.find(class_ = 'shingo-meisai-waku')
